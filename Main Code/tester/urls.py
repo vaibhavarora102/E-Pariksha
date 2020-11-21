6bbb94053c9e4c86c8b03f19c89fd0f2183390bf
devel-jot <jotroop2001@gmail.com>
2020-11-21 09:43:45 +0530

    added other pages

--- Main Code/tester/urls.py
+++ Main Code/tester/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 
 ]
 
--- Main Code/tester/views.py
+++ Main Code/tester/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'tester/index.html')
+def about(request):
+    return render(request, 'tester/about.html')
+def contact(request):
+    return render(request, 'tester/contact.html')
